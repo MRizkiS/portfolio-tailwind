0ce65fb488a8ec6ecf007d0fae296ced3f4cda52
MRizkiS <145929076+MRizkiS@users.noreply.github.com>
2023-09-30 00:01:22 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
@@ -14,7 +15,12 @@ module.exports = {
         wa: "#25D366",
         spotify: "#1DB954",
         X: "#000000",
-        dark: "#020617",
+        dark: "#000000",
+        html: "#E34F26",
+        css: "#1572B6",
+        bootstrap: "#7952B3",
+        tailwind: "#06B6D4",
+        kuning: "#F7DF1E",
       },
       screens: {
         "2xl": "1320px",
